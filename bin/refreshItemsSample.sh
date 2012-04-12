dbc1a0da124c340d218ea8772edea0f214204483
Howie Motenko <howie.motenko@jax.org>
2012-04-12 10:49:07 -0400

    used relative path

--- bin/refreshItemsSample.sh
+++ bin/refreshItemsSample.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #python ./dumpMgiItemXml.py -d ../output/mgi-sample -Dmoshfile=/Users/jer/work/mouseminedata/output/obo/MOSH.obo --logfile ./LOG --install Sampler
-python ./dumpMgiItemXml.py -d ../output/mgi-sample -Dmoshfile=${MM_DATA}/output/obo/MOSH.obo --logfile ./LOG --install Sampler
+python ./dumpMgiItemXml.py -d ../output/mgi-sample -Dmoshfile=../output/obo/MOSH.obo --logfile ./LOG --install Sampler
