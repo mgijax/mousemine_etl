e9ffaf37b7fcb5e63be4638c00a1e7e1f5075afd
Joel Richardson <joel.richardson@jax.org>
2012-08-08 08:04:52 -0400

    Dump url templates for DataSource objects.

--- bin/libdump/DataSourceDumper.py
+++ bin/libdump/DataSourceDumper.py
@@ -3,8 +3,11 @@ from AbstractItemDumper import *
 
 class DataSourceDumper(AbstractItemDumper):
     QTMPLT = '''
-    SELECT db._logicaldb_key, db.name, db.description
+    SELECT db._logicaldb_key, db.name, db.description, ab.name AS aname, ab.url
     FROM ACC_LogicalDB db
+      LEFT OUTER JOIN ACC_ActualDB ab
+      ON db._logicaldb_key = ab._logicaldb_key
+    ORDER BY db._logicaldb_key, ab._actualdb_key
     %(LIMIT_CLAUSE)s
     '''
 
@@ -12,14 +15,35 @@ class DataSourceDumper(AbstractItemDumper):
     <item class="DataSource" id="%(id)s">
       <attribute name="name" value="%(name)s" />
       <attribute name="description" value="%(description)s" />
+      %(url)s
       </item>
     '''
 
     def preDump(self):
         self.context.dataSourceByName = {}
+	q = '''
+	SELECT _logicaldb_key
+	FROM ACC_ActualDB
+	GROUP BY _logicaldb_key
+	HAVING count(*) > 1
+	'''
+	self.multActual = set()
+	for r in self.context.sql(q):
+	    self.multActual.add(r['_logicaldb_key'])
 
     def processRecord(self, r):
-	r['id'] = self.context.makeItemId('DataSource', r['_logicaldb_key'])
+	try:
+	    r['id'] = self.context.makeItemId('DataSource', r['_logicaldb_key'])
+	except:
+	    return None
+	if r['_logicaldb_key'] in self.multActual:
+	    r['name'] = r['aname']
+	if r['name'] == "MGI":
+	    r['url'] = 'http://www.informatics.jax.org/accession/@@@@'
+	if r['url'] is None:
+	    r['url'] = ''
+	else:
+	    r['url'] = '<attribute name="url" value="%(url)s" />' % r
 	self.context.dataSourceByName[r['name']] = r['id']
 	self.quoteFields(r, ['name','description'])
 	return r
