2dbabd9e847f828052e56e4c4d5c0686a4962d07
Steven Neuhauser <steven.neuhauser@jax.org>
2014-01-29 10:24:10 -0500

    Get EMAPA.obo file via ftp and reformat for loading

--- bin/refreshObo.sh
+++ bin/refreshObo.sh
@@ -14,6 +14,9 @@ TDIR=$BASEDIR/tmp
 # Extras/resources directory
 XDIR=$BASEDIR/resources
 
+# EMAPA (Abstract Mouse anatomy)
+curl ftp://ftp.informatics.jax.org/pub/custom/EMAPA.obo | python $BASEDIR/bin/libdump/ppEmapa.py > $ODIR/EMAPA.obo
+e=$?; if [ $e -ne 0 ]; then exit $e; fi
 
 # MEDIC
 curl https://gillnet.mdibl.org/~twiegers/mgi/mgiMEDIC.obo.gz | zcat > $ODIR/MEDIC.obo
@@ -34,17 +37,6 @@ e=$?; if [ $e -ne 0 ]; then exit $e; fi
 
 
 # UBERON
-curl -o $ODIR/uberon.obo http://obo.svn.sourceforge.net/svnroot/obo/uberon/trunk/uberon.obo
-e=$?; if [ $e -ne 0 ]; then exit $e; fi
-
-# EMAP (Edinburgh Anatomy)
-curl -o $ODIR/EMAP.obo ftp://ftp.hgu.mrc.ac.uk/pub/MouseAtlas/Anatomy/EMAP_combined.obo
-e=$?; if [ $e -ne 0 ]; then exit $e; fi
-
-# MA (adult mouse anatomy)
-curl -o $ODIR/MA.obo http://www.berkeleybop.org/ontologies/ma.obo
-e=$?; if [ $e -ne 0 ]; then exit $e; fi
-
-python $BASEDIR/bin/libdump/GXDAnatomyDumper.py $ODIR/MA.obo $ODIR/EMAP.obo $ODIR/EMAPX.obo
-e=$?; if [ $e -ne 0 ]; then exit $e; fi
+#curl -o $ODIR/uberon.obo http://obo.svn.sourceforge.net/svnroot/obo/uberon/trunk/uberon.obo
+#e=$?; if [ $e -ne 0 ]; then exit $e; fi
 
