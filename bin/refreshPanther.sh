122c6c204bda8f0d8469de03c21fd0cdd1db6c52
Howie Motenko <howie.motenko@jax.org>
2014-02-17 12:44:16 -0500

    update panther to load from current_release, rather than specific version

--- bin/refreshPanther.sh
+++ bin/refreshPanther.sh
@@ -7,7 +7,7 @@ then
     mkdir -p $ODIR
 fi
 
-curl -o $ODIR/RefGenomeOrthologs.tar.gz ftp://ftp.pantherdb.org/ortholog/8.0/RefGenomeOrthologs.tar.gz 
+curl -o $ODIR/RefGenomeOrthologs.tar.gz ftp://ftp.pantherdb.org/ortholog/current_release/RefGenomeOrthologs.tar.gz 
 e=$?; if [ $e -ne 0 ]; then exit $e; fi
 
 tar -xzvf $ODIR/RefGenomeOrthologs.tar.gz -C $ODIR
