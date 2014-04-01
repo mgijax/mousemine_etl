d6eab351f3faabe7a3b7d59b0534b887fe63feb0
Howie Motenko <howie.motenko@jax.org>
2014-04-01 11:21:18 -0400

    load secondary ids for markers and alleles

--- bin/libdump/SynonymDumper.py
+++ bin/libdump/SynonymDumper.py
@@ -15,6 +15,13 @@ class SynonymDumper(AbstractItemDumper):
     WHERE ml._orthologorganism_key is null
     AND ml.labeltype in ('MS','MN','MY')
     %(LIMIT_CLAUSE)s
+    ''','''
+    SELECT a.accid, a._mgitype_key, a._object_key
+    FROM ACC_Accession a
+    WHERE a._mgitype_key in (2,11)
+    AND a._logicaldb_key = 1
+    AND a.preferred = 0
+    AND a.private = 0
     ''']
 
     ITMPLT = '''
@@ -32,14 +39,19 @@ class SynonymDumper(AbstractItemDumper):
 	self.context.QUERYPARAMS['MGITYPEKEYS'] = ",".join(map(str,self.mgiTypeKeys))
 
     def processRecord(self, r, qindex):
-	if qindex == 0:
-	    r['id'] = self.context.makeItemId('Synonym')
+        if qindex == 0:
+            r['id'] = self.context.makeItemId('Synonym')
 	    r['value'] = self.quote(r['synonym'])
 	    r['subject'] = self.context.makeItemRef( r['_mgitype_key'], r['_object_key'])
 	    return r
-	else:
+	elif qindex == 1:
 	    r['id'] = self.context.makeItemId('Synonym')
 	    r['value'] = self.quote(r['label'])
 	    r['subject'] = self.context.makeItemRef( 'Marker', r['_marker_key'])
 	    return r
-
+        elif qindex == 2:
+            # load secondary ids for markers and alleles
+            r['id'] = self.context.makeItemId('Synonym')
+            r['value'] = self.quote(r['accid'])
+            r['subject'] = self.context.makeItemRef( r['_mgitype_key'], r['_object_key'])
+            return r
