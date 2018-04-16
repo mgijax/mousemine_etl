30ef144b2a1dc5770b862e298656a56e4385c9c5
Joel Richardson <joel.richardson@jax.org>
2018-04-16 15:20:55 -0400

    Second checkin. Convert MGI.gff3 file to use as C57BL/6J.

---
+++ bin/straindata/bin/config.sh
@@ -0,0 +1,18 @@
+
+export ENSEMBLRELEASE=91
+export BASEURL=ftp://ftp.ensembl.org/pub/release-${ENSEMBLRELEASE}/gff3
+#
+export BDIR="${DIR}/bin"
+export WDIR="${DIR}/work"
+export DDIR="${DIR}/downloads"
+export GDIR="${DIR}/gff"
+export SDIR="${DIR}/gff3.samples"
+#
+export LOGFILE="${WDIR}/LOG"
+#
+export STRAINS="${BDIR}/strains.tsv"
+export MAPPINGFILE="${WDIR}/mgiMapping.tsv"
+export VALIDIDFILE="${WDIR}/mgiIds.tsv"
+#
+source "${BDIR}/utils.sh"
+
---
+++ bin/straindata/bin/gff3lite.py
@@ -0,0 +1,76 @@
+#
+# gff3lite.py
+#
+
+import types
+
+#
+TAB	= '\t'
+NL	= '\n'
+SEMI	= ';'
+EQ	= '='
+HASH	= '#'
+BANG	= '!'
+COMMA	= ','
+#
+SEQID	= 0
+SOURCE	= 1
+TYPE	= 2
+START	= 3
+END	= 4
+SCORE	= 5
+STRAND	= 6
+PHASE	= 7
+ATTRIBUTES = 8
+#
+GFF3HEADER = "##gff-version 3"
+GFF3SEPARATOR = "###"
+#
+def parseCol9(s):
+    parts = dict([ part.split(EQ,1) for part in s.split(SEMI)])
+    return parts
+#
+def parseLine(line):
+    f = line.split(TAB)
+    f[8] = parseCol9(f[8])
+    return f
+#
+def formatCol9(attrs):
+    s = SEMI.join(['%s=%s'%(k,v) for (k,v) in attrs.items()])
+    return s
+#
+def formatLine(f):
+    f2 = list(f)
+    f2[8] = formatCol9(f2[8])
+    return TAB.join([str(x) for x in f2])
+
+def iterate(fileIn, yieldHeader=True, yieldGroups=True):
+    # open file
+    if type(fileIn) is types.StringType:
+        fin = open(fileIn, 'r')
+    else:
+        fin = fileIn
+    # collect header lines
+    header = []
+    line = fin.next()
+    while line.startswith(HASH):
+        header.append(line[:-1])
+	line = fin.next()
+    if yieldHeader:
+        yield header
+    firstGroup = True
+    currGroup = []
+    while line:
+        line = line[:-1]
+	if line == GFF3SEPARATOR:
+	    if len(currGroup) > 0:
+		if yieldGroups:
+		    yield currGroup
+		else:
+		    for f in currGroup:
+		        yield f
+		currGroup = []
+	else:
+	    currGroup.append(parseLine(line))
+	line = fin.next()
+    fin.close()
